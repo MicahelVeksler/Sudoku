c710f2c54803a377fd57d19e5caed735d2343c61
MicahelVeksler <146798900+MicahelVeksler@users.noreply.github.com>
2023-12-07 21:25:01 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,14 @@
+# Constant values for sudoku board
+screen_width = 540
+screen_height = 600
+cell_size = 60
+
+# Constant values for colors used in sudoku board
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+pink = (255, 0, 255)
+purple = (191, 62, 255)
+grey = (50, 50, 50)
+orange = (255, 97, 3)
+background = (164,211,238)
